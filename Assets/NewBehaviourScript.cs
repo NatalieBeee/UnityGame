30cc30f7286bdb1de2e1427f1769c48d943975a2
NatalieBeee <nataliebunting@outlook.com>
2019-12-17 19:31:48 +1100

    adding unity game

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+    public Rigidbody rb;
+
+    void Start()
+    {
+        //Debug.Log("UwU OwO ;-; TwT -_- @-@ :/");
+
+        //rb.useGravity = false;
+
+        rb.AddForce(0, 300, -666);
+    }
+
+    void Update()
+    {
+        //rb.AddForce(0, 300, -666);
+    }
+}
